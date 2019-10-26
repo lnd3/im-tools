5548f721ec628a8f80f46b69b8b805d4d38539e8
lnd3 <linus.cumselius@gmail.com>
2019-10-27 00:36:50 +0200

    Replace <br> and '\' 'n' with new line character so we'll be able to use the diff tool so we can make proper news updates.

--- imnewsall.sh
+++ imnewsall.sh
@@ -1,11 +1,30 @@
 #!/bin/bash
 
-newsfilename=$1
+filenameprefix=$1
+
+replaceNewlines() {
+  newline='\n'
+  newline2='\\n'
+  br='<br>'
+
+  string=$1
+  string1=${string//${br}/${newline}}
+  string2=${string1//${newline2}/${newline}}
+  echo -e ${string2}
+}
 
 ## get all pupil news
 pupilids=$(cat pupilids)
 for id in $pupilids; do
   ./imswitchpupil.sh ${id}
-  ./imnews.sh > ${newsfilename}${id}.html
-  ./imtimeline.sh > ${newsfilename}timeline${id}.html
+
+  ./imnews.sh > tmp1
+  ./imtimeline.sh > tmp2
+
+  file1=${filenameprefix}news${id}.txt
+  file2=${filenameprefix}timeline${id}.txt
+
+  replaceNewlines "$(cat tmp1)" > ${file1}
+  replaceNewlines "$(cat tmp2)" > ${file2}
+
 done
