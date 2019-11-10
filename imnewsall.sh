38b06c96461038da19b75324a6063537c50a8f9d
lnd3 <linus.cumselius@gmail.com>
2019-11-10 22:50:02 +0100

    Store backup of old data. Create pupil diffs and email those instead of a complete summary.

--- imnewsall.sh
+++ imnewsall.sh
@@ -10,17 +10,20 @@ removeFileIfExist update.txt
 ## get all pupil news
 pupilids=$(cat pupilids)
 for id in $pupilids; do
+  cp news${id}old.txt news${id}old_backup.txt
+  cp timeline${id}old.txt timeline${id}old_backup.txt
+
   ./imswitchpupil.sh ${id}
 
   ./imnews.sh > tmp
   replaceNewlines "$(cat tmp)" > news${id}.txt
-  diff --changed-group-format='%<%>' --unchanged-group-format='' news${id}old.txt news${id}.txt > newsdiff
+  diff --changed-group-format='%>' --unchanged-group-format='' news${id}old.txt news${id}.txt > newsdiff
   cp newsdiff news${id}update.txt
   cp news${id}.txt news${id}old.txt
 
   ./imtimeline.sh > tmp
   replaceNewlines "$(cat tmp)" > timeline${id}.txt
-  diff --changed-group-format='%<%>' --unchanged-group-format='' timeline${id}old.txt timeline${id}.txt > timelinediff
+  diff --changed-group-format='%>' --unchanged-group-format='' timeline${id}old.txt timeline${id}.txt > timelinediff
   cp timelinediff timeline${id}update.txt
   cp timeline${id}.txt timeline${id}old.txt
 
@@ -33,12 +36,33 @@ for id in $pupilids; do
     echo -e "\n" >> update-${timestring}.txt
     cat newsdiff >> update-${timestring}.txt
     cat timelinediff >> update-${timestring}.txt
+
+    cat newsdiff >> update${id}-${timestring}.txt
+    cat timelinediff >> update${id}-${timestring}.txt
+
+	if [ -s update${id}-${timestring}.txt ]
+	then
+	  emails=$(cat emails)
+	  {
+	    echo "To: ${emails}"
+	    echo "MIME-Version: 1.0"
+	    echo "Subject: Infomentor pupil ${id}, update ${timestring}"
+	    echo -e "$(cat update${id}-${timestring}.txt)"
+	  } | /usr/sbin/ssmtp ${emails}
+
+	  echo -e "Sent pupil ${id} update ${timestring}."
+	fi
+
   fi
 done
 
 removeFileIfExist tmp
 removeFileIfExist newsdiff
 removeFileIfExist timelinediff
+
+exit
+
+
 
 if [ -s update-${timestring}.txt ] && [ -s emails ]
 then
--- utils.sh
+++ utils.sh
@@ -14,6 +14,10 @@ replaceNewlines() {
   string=$1
   string1=${string//${br}/${newline}}
   string2=${string1//${newline2}/${newline}}
+# string3=${string2//}
+# regexp to find date: grep "([1-2][0-9]|[3][0-1]|[1-9])\/([1][0-2]|[1-9])""
+# find 12/16/09 etc: grep "^12\/1[6-8]\/09
+
   echo -e ${string2}
 }
 
